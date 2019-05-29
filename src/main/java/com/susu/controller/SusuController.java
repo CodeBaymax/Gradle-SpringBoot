6cda3d33a77a93e938d94860a3689f2a36a2157f
baymax <baymax0532@126.com>
2019-05-29 10:34:10 +0800

    springboot引入配置文件 LocalDate

--- src/main/java/com/susu/controller/SusuController.java
+++ src/main/java/com/susu/controller/SusuController.java
@@ -1,6 +1,7 @@
 package com.susu.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,9 +9,18 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 public class SusuController {
 
+    @Value("${su}")
+    private String su;
+
     @RequestMapping("/hey")
     public String hey() {
         log.info("susu");
         return "Hey, Susu";
     }
+
+    @RequestMapping("/conf")
+    public String suValue() {
+        return su;
+    }
+
 }
---
+++ src/main/java/com/susu/standardE/date/LocaleDateLearn.java
@@ -0,0 +1,9 @@
+package com.susu.standardE.date;
+
+import java.time.LocalDate;
+
+public class LocaleDateLearn {
+    public static void main(String[] args) {
+        System.out.println(LocalDate.now().toString());
+    }
+}

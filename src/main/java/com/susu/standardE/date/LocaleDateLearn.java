82cc03bf486637e96f1ee67ca0812d28f7c86ae6
baymax <baymax0532@126.com>
2019-06-05 16:02:53 +0800

    springboot引入配置文件 LocalDate

--- src/main/java/com/susu/standardE/date/LocaleDateLearn.java
+++ src/main/java/com/susu/standardE/date/LocaleDateLearn.java
@@ -1,9 +1,20 @@
 package com.susu.standardE.date;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 public class LocaleDateLearn {
     public static void main(String[] args) {
+
         System.out.println(LocalDate.now().toString());
+        System.out.println(LocalDate.now().minusDays(1).toString());
+
+        System.out.println(LocalDateTime.now().withNano(0));
+        System.out.println(LocalDateTime.now());
+
+        System.out.println(LocalTime.now());
+        System.out.println(LocalTime.now().withNano(0));
+
     }
 }
